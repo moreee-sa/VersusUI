52d62859d46f8085915ba2f92bec48a798b6602b
Diverse41 <morenolms41@gmail.com>
2025-04-27 15:24:09 +0200

    Spostata l'animazione nel file CSS separato

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,13 @@
 import styled from 'styled-components';
 import Navbar from './Components/Navbar';
 import HeroSection from './Components/HeroSection';
+import './home.css'
 
 const Homepage = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  background: linear-gradient(270deg, #1A120B, #3C2A21, #D5CEA3);
-  background-size: 600% 600%;
-  animation: GradientAnimation 20s ease infinite;
-
-  @keyframes GradientAnimation {
-    0% { background-position: 0% 50%; }
-    50% { background-position: 100% 50%; }
-    100% { background-position: 0% 50%; }
-  }
 `;
 
 const HowTo = styled.div`
