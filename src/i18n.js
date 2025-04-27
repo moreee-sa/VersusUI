fed398f7809a099249198e9623d6700dcd4dc2d9
Diverse41 <morenolms41@gmail.com>
2025-04-27 14:59:38 +0200

    Aggiunta la funzionalità per il caricamento delle traduzioni da file JSON

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpBackend from 'i18next-http-backend';
 
 i18n
   .use(LanguageDetector)
+  .use(HttpBackend)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
@@ -11,48 +13,13 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-    "resources": {
-      "it": {
-        "translation": {
-          "realTimeScore": "Una sovrapposizione di punteggio in tempo reale per sfide amichevoli 1vs1 e scontri a squadre.",
-          "startOverlay": "Apri sovrapposizione",
-          "helpGuide": "Hai bisogno di aiuto? Consulta la guida! (Presto)",
-          "comingSoon": "In arrivo",
-          "guide": {
-            "introduction": {
-              "title": "Introduzione",
-              "content": "Benvenuto nella guida introduttiva! Qui troverai tutte le informazioni base per cominciare."
-            },
-            "setup": {
-              "title": "Setup per OBS",
-              "content": "Questa guida ti aiuterà a configurare OBS per il miglior utilizzo del nostro overlay."
-            }
-          }
-        }
-      },
-      "en": {
-        "translation": {
-          "realTimeScore": "A Real-time score overlay for friendly 1vs1 and team face-offs.",
-          "startOverlay": "Open Overlay",
-          "helpGuide": "Need help? Check the guide! (Soon)",
-          "comingSoon": "Coming soon",
-          "guide": {
-            "introduction": {
-              "title": "Introduction",
-              "content": "Welcome to the introduction guide! Here you'll find all the basic information to get started."
-            },
-            "setup": {
-              "title": "Setup for OBS",
-              "content": "This guide will help you set up OBS for the best use of our overlay."
-            }
-          }
-        }
-      }
-    },    
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
     detection: {
       order: ['navigator'],
       caches: [],
-    }
+    },
   });
 
 export default i18n;
